c8f01cc3938cd1180da243bd332019cf55b9145b
C-Lion Stepansky <esstepansky@gmail.com>
2021-05-20 23:38:00 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,16 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  def key_for_min_value(hash)
+  smallest_key = ""
+  smallest_value = ""
+  hash.each do |key, value|
+    if lowest_value == "" || value < smallest_value
+      lowest_value = v
+      lowest_key = k
+    end
+  end
+  lowest_key
 end
+    
+  
